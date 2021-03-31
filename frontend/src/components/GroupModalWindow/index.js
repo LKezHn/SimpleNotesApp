babc7b366c40d56fbe70ab91feb122d332e1c0b8
Luis <mluise2406@gmail.com>
2021-03-31 13:31:37 -0600

    context added

--- frontend/src/components/GroupModalWindow/index.js
+++ frontend/src/components/GroupModalWindow/index.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Modal from 'react-modal';
 import axios from 'axios';
 import { useEffect } from 'react';
 
-function GroupModalWindow({ status, id }) {
+import NotesContext from '../../context/Notes/NotesContext'
 
+function GroupModalWindow({ id }) {
+
+  const { modalGroupStatus, closeGroupModal} = useContext(NotesContext);
   const apiUri = 'http://localhost:4000/api/v1/groups';
   const token = localStorage.getItem('token');
 
   const [groupInfo, setGroupInfo] = useState({})
   const [members, setMembers] = useState([])
   const [groupNotes, setGroupNotes] = useState([])
+  const [newNote, setNewNote] = useState({ noteTitle: "", noteDescription: "" })
+
 
   useEffect(() => {
+    setGroups(id, apiUri)
+  }, [])
+
+  const setGroups = (id, apiUri) => {
     axios.get(`${apiUri}/${id}`, {
       headers: {
         'x-access-token': token
@@ -23,14 +32,32 @@ function GroupModalWindow({ status, id }) {
       setGroupNotes(res.data.notes)
     }
     ).catch(err => console.error(err))
-  }, [])
+  }
+
+  const handleNewNote = (e) => {
+    setNewNote({...newNote, [e.target.name]: e.target.value})
+  }
+
+  const addNewGroupNote = (e) => {
+    e.preventDefault()
+    console.log(newNote)
+    axios.post(`${apiUri}/${groupInfo.code}/newNote`, newNote, {
+      headers: {
+        'x-access-token': token
+      }
+    }).then( (res) =>{
+      setNewNote({noteTitle:"", noteDescription: ""})
+      setGroups(id, apiUri)
+    }
+    ).catch( err => console.error(err))
+  }
 
   return (
     <Modal
       ariaHideApp={false}
-      isOpen={status}
+      isOpen={modalGroupStatus}
       contentLabel='Groups'
-      onRequestClose={() => { return false }}
+      onRequestClose={ closeGroupModal }
       style={{
         overlay: {
           backgroundColor: 'rgba(255, 255, 255, 0.5)'
@@ -46,8 +73,22 @@ function GroupModalWindow({ status, id }) {
     >
       <h1 className="text-xl text-center font-semibold text-purple-600">{groupInfo.name}</h1>
       <p className='text-center text-md text-gray-600 font-medium'>Code: {groupInfo.code}</p>
+      <div className='my-4 mx-2'>
+        <button className='text-center my-1 mr-0 px-2 py-1 bg-red-500 text-white rounded' onClick={closeGroupModal }>Close</button>
+        <h1 className='text-center bg-purple-400 text-white rounded'>Add a note</h1>
+        <form className='px-auto ustify-center'>
+          <input className='justify-center bg-gray-300 w-full px-3 py-1 my-2 text-purple-600' type='text' name='noteTitle' value={newNote.noteTitle} onChange={handleNewNote} placeholder='Note title' />
+          <textarea
+            placeholder='Note Description'
+            name='noteDescription'
+            value={newNote.noteDescription}
+            onChange={handleNewNote}
+            className='bg-gray-300 w-full h-24 px-3 py-1 text-purple-600 text-sm font-medium rounded resize-none' />
+        </form>
+          <button type='button' onClick={addNewGroupNote} className='my-2 block text-sm bg-purple-500 text-white px-3 py-1 rounded mx-auto'>Add Note</button>
+      </div>
       <div className='flex flex-row'>
-        <div className='w-1/3'>
+        <div className='w-1/4'>
           <h1 className='bg-purple-400 text-white rounded m-1 text-center' onClick={() => console.log(groupNotes)}>Users</h1>
           {
             members.map((member) => (
@@ -55,11 +96,11 @@ function GroupModalWindow({ status, id }) {
               ) )
             }
         </div>
-        <div className='w-2/3'>
+        <div className='w-3/4'>
           <h1 className='bg-purple-400 text-white rounded m-1 text-center'>Notes</h1>
             {
               groupNotes.length > 0 && groupNotes.map(( note ) => (
-                <div className='bg-white border mx-2 border-purple-600 rounded text-gray-600 font-medium px-2' key={note._id}>
+                <div className='bg-white border mx-2 my-1 border-purple-600 rounded text-gray-600 font-medium px-2' key={note._id}>
                   <h1 className='text-purple-500 text-md'>{note.noteTitle}</h1>
                   <h3 className=' text-gray-600 text-sm font-light'>Author: {note.author.username}</h3>
                 </div>
--- frontend/src/components/Groups/index.js
+++ frontend/src/components/Groups/index.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 import GroupModalWindow from '../GroupModalWindow';
+import JoinModal from '../JoinModal'
+
+import NotesContext from '../../context/Notes/NotesContext';
 
 function Groups() {
+
+  const { openGroupModal, openJoinModal } = useContext(NotesContext);
 
   const apiUri = 'http://localhost:4000/api/v1/groups';
   const token = localStorage.getItem('token');
 
   const [groupList, setGroupList] = useState([])
-  const [status, setStatus] = useState(false)
 
   useEffect(() => {
     axios.get(apiUri, {
@@ -22,9 +26,13 @@ function Groups() {
     )
   }, [])
 
-  const openModal = (e) => {
-    e.preventDefault();
-    setStatus(true)
+  const open = (e) => {
+    e.preventDefault()
+    openGroupModal()
+  }
+
+  const joinModal = () => {
+    openJoinModal()
   }
 
   return (
@@ -34,14 +42,17 @@ function Groups() {
       </div>
       {
         groupList.length > 0 && groupList.map((group) => (
-          <div key={group._id} onClick={openModal} className='mx-2 my-1 py-1 px-4 rounded border border-purple-600 bg-gray-100 cursor-pointer text-left'>
+          <div key={group._id} onClick={open} className='mx-2 my-1 py-1 px-4 rounded border border-purple-600 bg-gray-100 cursor-pointer text-left'>
             <span className='text-sm font-normal text-purple-900'>{group.name}</span><br/>
             <span className='text-xs font-light text-gray-600'>{group.code}</span>
-            <GroupModalWindow status={status} id={group.code}/>
+            <GroupModalWindow  id={group.code}/>
           </div>
         ))
       }
       { groupList.length == 0 && <p className=' text-center font-medium text-purple-500 '>You have not any group.</p>} 
+      <p className='px-2 text-purple-600 text-center text-sm cursor-pointer' onClick={joinModal}>Join to a group</p>
+      <JoinModal />
+      {/* <p className='px-2 text-purple-600 text-center text-sm cursor-pointer' onClick={createGroupModal}>Create a group</p> */}
     </div>
   )
 }
